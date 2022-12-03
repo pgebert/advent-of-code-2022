1e32b29738c4c0bfdaa4c5a1ec28588e76f66e93
Patrick Gebert <patrick.gebert@web.de>
2022-12-03 20:42:58 +0100

    Added solution for day 1 part 2.

---
+++ tests/day_01/day_01_part_2_test.py
@@ -0,0 +1,33 @@
+from problems.day_01 import day_01_part_2
+from utils import read_lines_from_comment, read_lines_from_file
+
+
+def test_day_01_part_2_example():
+    example = """
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+    """
+
+    input = read_lines_from_comment(example, True)
+    result = day_01_part_2.solve(input)
+
+    assert 45000 == result
+
+
+def test_day_01_part_2_problem():
+    input = read_lines_from_file("..\\..\\data\\day_01\\day_01_input.txt", True)
+    result = day_01_part_2.solve(input)
+
+    assert 213089 == result
