b02de537b04389e1323c5bdef8f13c2fe3ffae21
Patrick Gebert <patrick.gebert@web.de>
2022-12-12 21:23:14 +0100

    "Added solution for day 12."

--- problems/day_12/day_12_part_1.py
+++ problems/day_12/day_12_part_1.py
@@ -2,25 +2,51 @@
 
 https://adventofcode.com/2022/day/12
 
+--- Day 12: Hill Climbing Algorithm ---
+You try contacting the Elves using your handheld device, but the river you're following must be too low to get a decent signal.
+
+You ask the device for a heightmap of the surrounding area (your puzzle input). The heightmap shows the local area from above broken into a grid; the elevation of each square of the grid is given by a single lowercase letter, where a is the lowest elevation, b is the next-lowest, and so on up to the highest elevation, z.
+
+Also included on the heightmap are marks for your current position (S) and the location that should get the best signal (E). Your current position (S) has elevation a, and the location that should get the best signal (E) has elevation z.
+
+You'd like to reach E, but to save energy, you should do it in as few steps as possible. During each step, you can move exactly one square up, down, left, or right. To avoid needing to get out your climbing gear, the elevation of the destination square can be at most one higher than the elevation of your current square; that is, if your current elevation is m, you could step to elevation n, but not to elevation o. (This also means that the elevation of the destination square can be much lower than the elevation of your current square.)
+
+For example:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Here, you start in the top-left corner; your goal is near the middle. You could start by moving down or right, but eventually you'll need to head toward the e at the bottom. From there, you can spiral around to the goal:
+
+v..v<<<<
+>v.vv<<^
+.>vv>E^^
+..v>>>^^
+..>>>>>^
+In the above diagram, the symbols indicate whether the path exits each square moving up (^), down (v), left (<), or right (>). The location that should get the best signal is still E, and . marks unvisited squares.
+
+This path reaches the goal in 31 steps, the fewest possible.
+
+What is the fewest steps required to move from your current position to the location that should get the best signal?
+
 
 
 """
 
 import string
 from sys import maxsize
-from typing import List
+from typing import List, Tuple
 
 
 def toInt(character: str) -> int:
-    value = -1
     if character == "S":
-        value = 1
+        return 1
     elif character == "E":
-        value = 26
-    else:
-        value = string.ascii_letters.index(character) + 1
+        return 26
 
-    return value
+    return string.ascii_letters.index(character) + 1
 
 
 class Area:
@@ -37,7 +63,10 @@ class Area:
             if "E" in line:
                 self.destination = (row, line.index("E"))
 
-    def cost_to_destionation(self, iterations: int = 800):
+    def access(self, start: Tuple[int, int], destionation: Tuple[int, int]) -> bool:
+        return self.matrix[start[0]][start[1]] >= self.matrix[destionation[0]][destionation[1]] - 1
+
+    def cost_to_destionation(self):
 
         cost = []
         for row in self.matrix:
@@ -46,7 +75,8 @@ class Area:
         start_row, start_col = self.start
         cost[start_row][start_col] = 0
 
-        for _ in range(iterations):
+        while True:
+            changed = False
             for y in range(len(self.matrix)):
                 for x in range(len(self.matrix[y])):
 
@@ -54,15 +84,18 @@ class Area:
                         continue
 
                     updated_cost = 1 + min(
-                        cost[y - 1][x] if y > 0 and self.matrix[y - 1][x] >= self.matrix[y][x] - 1 else maxsize,
-                        cost[y][x - 1] if x > 0 and self.matrix[y][x - 1] >= self.matrix[y][x] - 1 else maxsize,
-                        cost[y + 1][x] if y < len(self.matrix) - 1 and self.matrix[y + 1][x] >= self.matrix[y][
-                            x] - 1 else maxsize,
-                        cost[y][x + 1] if x < len(self.matrix[y]) - 1 and self.matrix[y][x + 1] >= self.matrix[y][
-                            x] - 1 else maxsize
+                        cost[y - 1][x] if y > 0 and self.access((y - 1, x), (y, x)) else maxsize,
+                        cost[y][x - 1] if x > 0 and self.access((y, x - 1), (y, x)) else maxsize,
+                        cost[y + 1][x] if y < len(self.matrix) - 1 and self.access((y + 1, x), (y, x)) else maxsize,
+                        cost[y][x + 1] if x < len(self.matrix[y]) - 1 and self.access((y, x + 1), (y, x)) else maxsize
                     )
 
-                    cost[y][x] = min(updated_cost, cost[y][x])
+                    if updated_cost < cost[y][x]:
+                        cost[y][x] = updated_cost
+                        changed = True
+
+            if not changed:
+                break
 
         destination_row, destination_col = self.destination
         return cost[destination_row][destination_col]
--- problems/day_12/day_12_part_2.py
+++ problems/day_12/day_12_part_2.py
@@ -2,13 +2,34 @@
 
 https://adventofcode.com/2022/day/12
 
+--- Part Two ---
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
 
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+
+Again consider the example from above:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+This path reaches the goal in only 29 steps, the fewest possible.
+
+What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
 
 """
 
 import string
 from sys import maxsize
-from typing import List
+from typing import List, Tuple
 
 
 def toInt(character: str) -> int:
@@ -24,62 +45,53 @@ class Area:
 
     def __init__(self, input: List[str]):
         self.matrix = []
-        self.starts = []
-        self.destination = (0, 0)
+        self.start = (0, 0)
+        self.destinations = []
 
         for row, line in enumerate(input):
             self.matrix.append([toInt(s) for s in line])
 
             for col, character in enumerate(line):
                 if character == "S" or character == "a":
-                    self.starts.append((row, col))
+                    self.destinations.append((row, col))
                 elif character == "E":
-                    self.destination = (row, col)
+                    self.start = (row, col)
+
+    def access(self, start: Tuple[int, int], destionation: Tuple[int, int]) -> bool:
+        return self.matrix[start[0]][start[1]] <= self.matrix[destionation[0]][destionation[1]] + 1
 
     def cost_to_destionation(self):
 
-        min_path = maxsize
-        destination_row, destination_col = self.destination
+        cost = []
+        for row in self.matrix:
+            cost.append([maxsize for _ in row])
 
-        for i, (start_row, start_col) in enumerate(self.starts):
+        start_row, start_col = self.start
+        cost[start_row][start_col] = 0
 
-            cost = []
-            for row in self.matrix:
-                cost.append([maxsize for _ in row])
+        while True:
+            changed = False
+            for y in range(len(self.matrix)):
+                for x in range(len(self.matrix[y])):
 
-            cost[start_row][start_col] = 0
+                    if x == 0 and y == 0:
+                        continue
 
-            while True:
-                changed = False
-                for y in range(len(self.matrix)):
-                    for x in range(len(self.matrix[y])):
+                    updated_cost = 1 + min(
+                        cost[y - 1][x] if y > 0 and self.access((y - 1, x), (y, x)) else maxsize,
+                        cost[y][x - 1] if x > 0 and self.access((y, x - 1), (y, x)) else maxsize,
+                        cost[y + 1][x] if y < len(self.matrix) - 1 and self.access((y + 1, x), (y, x)) else maxsize,
+                        cost[y][x + 1] if x < len(self.matrix[y]) - 1 and self.access((y, x + 1), (y, x)) else maxsize
+                    )
 
-                        if x == 0 and y == 0:
-                            continue
+                    if updated_cost < cost[y][x]:
+                        cost[y][x] = updated_cost
+                        changed = True
 
-                        updated_cost = 1 + min(
-                            cost[y - 1][x] if y > 0 and self.matrix[y - 1][x] >= self.matrix[y][x] - 1 else maxsize,
+            if not changed:
+                break
 
-                            cost[y][x - 1] if x > 0 and self.matrix[y][x - 1] >= self.matrix[y][x] - 1 else maxsize,
-                            cost[y + 1][x] if y < len(self.matrix) - 1 and self.matrix[y + 1][x] >= self.matrix[y][
-                                x] - 1 else maxsize,
-                            cost[y][x + 1] if x < len(self.matrix[y]) - 1 and self.matrix[y][x + 1] >= self.matrix[y][
-                                x] - 1 else maxsize
-                        )
-
-                        if updated_cost < cost[y][x]:
-                            cost[y][x] = updated_cost
-                            changed = True
-
-                if not changed:
-                    break
-
-            if (new_min_path := cost[destination_row][destination_col]) < min_path:
-                min_path = new_min_path
-
-            print(f"{i + 1} from {len(self.starts)}: min path: {min_path}")
-
-        return min_path
+        return min([cost[row][col] for row, col in self.destinations])
 
 
 def solve(input: List[str]):
