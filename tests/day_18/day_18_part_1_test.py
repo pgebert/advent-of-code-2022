6e6458897c3a864a81ce3d0d4195b32bbd18fcff
Patrick Gebert <patrick.gebert@web.de>
2022-12-18 13:41:05 +0100

    "Added solution for day 18 part 1."

--- tests/day_18/day_18_part_1_test.py
+++ tests/day_18/day_18_part_1_test.py
@@ -2,28 +2,43 @@ from problems.day_18 import day_18_part_1
 from utils import read_lines_from_comment, read_lines_from_file
 
 
-def test_day_18_part_1_example():
+def test_day_18_part_1_example_1():
     example = """
-        [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-        [[[5,[2,8]],4],[5,[[9,9],0]]]
-        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-        [[[[5,4],[7,7]],8],[[8,3],8]]
-        [[9,3],[[9,9],[6,[4,9]]]]
-        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+        1,1,1
+        2,1,1
     """
 
     input = read_lines_from_comment(example)
     result = day_18_part_1.solve(input)
 
-    assert 4140 == result
+    assert result == 10
+
+
+def test_day_18_part_1_example_2():
+    example = """
+        2,2,2
+        1,2,2
+        3,2,2
+        2,1,2
+        2,3,2
+        2,2,1
+        2,2,3
+        2,2,4
+        2,2,6
+        1,2,5
+        3,2,5
+        2,1,5
+        2,3,5
+    """
+
+    input = read_lines_from_comment(example)
+    result = day_18_part_1.solve(input)
+
+    assert result == 64
 
 
 def test_day_18_part_1_problem():
     input = read_lines_from_file(".\\data\\day_18\\day_18_input.txt")
     result = day_18_part_1.solve(input)
 
-    assert 3691 == result
+    assert result == 4320
